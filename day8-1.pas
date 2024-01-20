1e79741adfc8013a4e9dd5f845058587a7ea26b1
psychotimmy <timholyoake@btopenworld.com>
2024-01-20 12:08:45 +0000

    6x speed improvement
    6x speed improvement by adding numeric pointers to the next left and right elements in the node array. Still sub-optimal however!

--- day8-1.pas
+++ day8-1.pas
@@ -4,18 +4,19 @@ type
   str16 = string[16];
   node =
     record
-      name : str3;
-      left : str3;
-      right : str3
+      name: str3;
+      left: str3;
+      right: str3;
+      lno: integer;
+      rno: integer
     end;
 var
   nodefile: text;
   line: str16;
   turns: array[1..280] of char;
-  map: array[1..760] of node;
+  map: array[1..750] of node;
   thisnode: node;
-  nextname: str3;
-  total, numturns,idx, nptr: integer;
+  total, numturns,idx: integer;
   direction: char;
 
 procedure getturns(var len: integer);
@@ -36,13 +37,34 @@ end;
 
 procedure addnode(ln:str16; id:integer);
 begin
+    if (id mod 150) = 0 then
+      writeln('Adding map node ',id);
     map[id].name := copy(ln,1,3);
     map[id].left := copy(ln,8,3);
     map[id].right := copy(ln,13,3)
 end;
 
+procedure completemap(nt:integer);
+var id, i: integer;
+begin
+  for id := 1 to nt do
+  begin
+    if (id mod 150) = 0 then
+      writeln('Completing map node ',id);
+    i := 1;
+    while map[i].name <> map[id].left do
+      i := i+1;
+    map[id].lno := i;
+    i := 1;
+    while map[i].name <> map[id].right do
+      i := i+1;
+    map[id].rno := i
+  end
+end;
+
 begin
   writeln('Advent of Code 2023 Day 8, part 1');
+  writeln('');
   assign(nodefile,'day8in.txt');
   reset(nodefile);
   getturns(numturns); { Left / right turn sequence is first line of file }
@@ -58,11 +80,16 @@ begin
   end;
   close(nodefile);
 
+  { Complete the map to improve walk speed }
+  writeln('');
+  completemap(idx-1);
+
   { Get AAA node to start }
-  nptr := 1;
-  while map[nptr].name <> 'AAA' do
-    nptr := nptr+1;
-  thisnode := map[nptr];
+  writeln('');
+  idx := 1;
+  while map[idx].name <> 'AAA' do
+    idx := idx+1;
+  thisnode := map[idx];
 
   { Go to the ZZZ node following the instructions }
   total := 0;
@@ -70,15 +97,12 @@ begin
   begin
     direction := turns[(total mod numturns)+1];
     if direction = 'L' then
-      nextname := thisnode.left
+      idx := thisnode.lno
     else
-      nextname := thisnode.right;
-    nptr := 1;
-    while map[nptr].name <> nextname do
-      nptr := nptr+1;
-    thisnode := map[nptr];
+      idx := thisnode.rno;
+    thisnode := map[idx];
     total := total+1;
-    if (total mod 500) = 0 then
+    if (total mod 1000) = 0 then
       writeln(total,' steps taken so far')
   end;
 
